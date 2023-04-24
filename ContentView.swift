13a22c3d6604a780f1a11fe7f0230c215420bceb
Sam Gebhardt <sgebhardt6133@stu.d214.org>
2023-04-24 13:15:29 -0500

    struct

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State var workout:String = ""
+    @State var Sets:Integer = 0
+    @State var Reps
     var body: some View {
         VStack {
             TextField("Enter Workout", text: $workout)
---
+++ Struct.swift
@@ -0,0 +1,16 @@
+//
+//  Struct.swift
+//  WorkoutTracker
+//
+//  Created by sam gebhardt on 04/02/1402 AP.
+//
+
+import SwiftUI
+
+struct Exercise: Hashable {
+    
+    var Workout:String
+    var Sets:Int
+    var Reps:Int
+    var Weight:Int
+}
