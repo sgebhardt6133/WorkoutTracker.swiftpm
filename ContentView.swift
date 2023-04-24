67f8928905b8d4ce6eba7b4eb4181bb6c0792fd8
Sam Gebhardt <sgebhardt6133@stu.d214.org>
2023-04-24 12:46:41 -0500

    test

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "square")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
