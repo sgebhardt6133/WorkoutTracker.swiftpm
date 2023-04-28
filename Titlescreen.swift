9fa9bd1f505851390177c3bc1a56dd675e8901d0
Nathan Marotta <nmarotta6249@stu.d214.org>
2023-04-28 12:43:07 -0500

    .

---
+++ Titlescreen.swift
@@ -0,0 +1,17 @@
+//
+//  Titlescreen.swift
+//  WorkoutTracker
+//
+//  Created by Nathan Marotta  on 4/28/23.
+//
+
+import SwiftUI
+
+struct TitleScreen: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
