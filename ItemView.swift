8339c2c5967ff5e64e27a148dc4d77922cf9d049
Sam Gebhardt <sgebhardt6133@stu.d214.org>
2023-05-04 12:18:43 -0500

    c

--- ItemView.swift
+++ ItemView.swift
@@ -11,15 +11,16 @@ struct MyView: View {
     let CurrentItem: Exercise
     var body: some View {
         HStack{
-//                        Text(currentitem.Subject)
-//                    .font(.largeTitle)
-//                        Text("||")
-//                    .font(.largeTitle)
-//                        Text("\(currentitem.Description)")
-//                    .font(.title3)
-//                        Text("||")
-//                            .font(.largeTitle)
-//                        Text("\(currentitem.DueDate)")
-//                    .font(.title3)
+            //                        Text(currentitem.Subject)
+            //                    .font(.largeTitle)
+            //                        Text("||")
+            //                    .font(.largeTitle)
+            //                        Text("\(currentitem.Description)")
+            //                    .font(.title3)
+            //                        Text("||")
+            //                            .font(.largeTitle)
+            //                        Text("\(currentitem.DueDate)")
+            //                    .font(.title3)
+        }
     }
 }
